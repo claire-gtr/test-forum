8aa272809a7162804fe10ea9c9f45ea637df1a4c
claire gautier <claire.gautier@edhec.com>
2020-04-27 16:50:21 +0200

    addes rails ujs config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -9,7 +9,8 @@ environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
+    Popper: ['popper.js', 'default'],
+    Rails: ['@rails/ujs']
   })
 );
 environment.loaders.prepend('erb', erb)
